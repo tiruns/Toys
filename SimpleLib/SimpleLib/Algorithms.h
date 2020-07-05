e4399f6a54dccffd137b7ae5c0489b4bacd63eab
tiruns <tiruns@yeah.net>
2020-07-05 20:33:20 +0800

    fix typo; add QuickSort

--- SimpleLib/SimpleLib/Algorithms.h
+++ SimpleLib/SimpleLib/Algorithms.h
@@ -1,9 +1,21 @@
 #pragma once
 
 #include <vector>
+#include <cstdlib>
 
 template <class Iter>
-void QiuckSort(Iter begin, Iter end)
+void QuickSort(Iter begin, Iter end)
 {
-	
+	if (begin < end)
+	{
+		auto r = end - 1;
+		auto rval = *r;
+		auto l = begin;
+		for (auto i = begin; i < r; ++i)
+			if (*i < rval)
+				std::swap(*i, *l++);
+		std::swap(*l, *r);
+		QuickSort(begin, l);
+		QuickSort(l + 1, end);
+	}
 }
--- SimpleLib/Tests/main.cpp
+++ SimpleLib/Tests/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 
 #include <BinaryHeap.h>
+#include <Algorithms.h>
 #pragma comment(lib, "SimpleLib.lib")
 
 using namespace std;
@@ -13,8 +14,17 @@ using namespace std;
 class BasicTest
 {
 public:
-	virtual string GetName() = 0;
+	vector<int> GenerateRandomNumbers(int num = 10, int min = -100, int max = 100)
+	{
+		vector<int> result;
+		auto randomDevice = mt19937();
+		auto uniformDist = uniform_int_distribution<int>(min, max);
+		for (int i = 0; i < num; ++i)
+			result.push_back(uniformDist(randomDevice));
+		return result;
+	}
 public:
+	virtual string GetName() = 0;
 	virtual bool Run() = 0;
 };
 
@@ -28,10 +38,8 @@ public:
 
 	virtual bool Run() override
 	{
-		auto testCase = vector<int>();
-		for (int i = -5; i <= 5; ++i)
-			testCase.push_back(i);
-		shuffle(testCase.begin(), testCase.end(), mt19937(random_device()()));
+		auto testCase = this->GenerateRandomNumbers();
+		//shuffle(testCase.begin(), testCase.end(), mt19937(random_device()()));
 
 		BinaryHeap<int, std::greater<int>> binHeap;
 		for (auto val : testCase)
@@ -49,10 +57,39 @@ public:
 	}
 };
 
+
+class QuickSortTest : public BasicTest
+{
+public:
+	virtual string GetName() override
+	{
+		return "QuickSortTest";
+	}
+	virtual bool Run() override
+	{
+		auto testCase = this->GenerateRandomNumbers();
+		auto dup = testCase;
+		QuickSort(testCase.begin(), testCase.end());
+		sort(dup.begin(), dup.end());
+
+		if (testCase.size() != dup.size())
+			return false;
+
+		for (int i = 0; i < testCase.size(); ++i)
+		{
+			if (testCase[i] != dup[i])
+				return false;
+		}
+
+		return true;
+	}
+};
+
 int main()
 {
 	vector<unique_ptr<BasicTest>> tests;
 	tests.push_back(make_unique<BinaryHeapTest>());
+	tests.push_back(make_unique<QuickSortTest>());
 
 	int passCounter = 0;
 

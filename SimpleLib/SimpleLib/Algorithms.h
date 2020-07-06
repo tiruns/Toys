709addb8263e093bb42d0b3d1ef71a8e5a52834c
tiruns <tiruns@yeah.net>
2020-07-06 21:01:07 +0800

    make QuickSort tail recursive

--- SimpleLib/SimpleLib/Algorithms.h
+++ SimpleLib/SimpleLib/Algorithms.h
@@ -4,20 +4,16 @@
 #include <random>
 #include <cstdlib>
 
-namespace SimpleLibInternal
-{
-	auto sRandomDevice = std::mt19937_64(std::random_device()());
-	auto sUniformSizeTDist = std::uniform_int_distribution<size_t>();
-}
-
 template <class Iter>
 void QuickSort(Iter begin, Iter end)
 {
-	if (begin < end)
+	static auto sRandomDevice = std::mt19937_64(std::random_device()());
+	static auto sUniformSizeTDist = std::uniform_int_distribution<size_t>();
+
+	while (begin < end)
 	{
 		// TODO: a more elegent approach to generate random numbers
-		auto rnd = SimpleLibInternal::sUniformSizeTDist(SimpleLibInternal::sRandomDevice) % (end - begin);
-		
+		auto rnd = sUniformSizeTDist(sRandomDevice) % (end - begin);
 		std::swap(*(begin + rnd), *(end - 1));
 
 		auto r = end - 1;
@@ -28,6 +24,6 @@ void QuickSort(Iter begin, Iter end)
 				std::swap(*i, *l++);
 		std::swap(*l, *r);
 		QuickSort(begin, l);
-		QuickSort(l + 1, end);
+		begin = l + 1;
 	}
 }

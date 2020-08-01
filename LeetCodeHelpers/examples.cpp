7333fb423284f04f70fece889669c7fe36e624a0
tiruns <tiruns@yeah.net>
2020-08-01 15:42:08 +0800

    fix indent

--- LeetCodeHelpers/examples.cpp
+++ LeetCodeHelpers/examples.cpp
@@ -13,7 +13,7 @@ int main()
 {
 	auto nums = helpers::parse_integers("[0, -1, 2, -3]");
 	auto strs = helpers::parse_strings(R"(["aaa", "bbbb", "cdefg"])");
-    auto matrix = helpers::parse_matrix("[[1,-2,3], [-4,5,-6], [-7,8,-9]]");
+	auto matrix = helpers::parse_matrix("[[1,-2,3], [-4,5,-6], [-7,8,-9]]");
 
 	auto btree = helpers::parse_btree("[0, 1, 2, null, null, 5, 6]");
 	Solution().btreeProblem(&btree.front());

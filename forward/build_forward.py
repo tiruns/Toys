f7e4a80cb4e00bde1d1e8bda6dbc52622a2fef4d
tiruns <tiruns@yeah.net>
2021-09-12 23:38:37 +0800

    update build_forward.py

--- forward/build_forward.py
+++ forward/build_forward.py
@@ -1,7 +1,45 @@
 import os
 import sys
+import subprocess
 
-CompilerPath = "cl.exe"
+CompilerPath = r'cl.exe'
+Definitions = [
+    r'UNICODE',
+    r'_UNICODE',
+    r'O2',
+    r'NDEBUG',
+    r'USE_NAMED_MUTEX'
+]
+IncludesPaths = []
+LibPaths = []
+
+def AddIncludePath(path):
+    return r'/I "{}" '.format(path)
+
+def AddLibPath(path):
+    return r'/LIBPATH:"{}" '.format(path)
+
+def AddDefinition(d):
+    return r'/D "{}" '.format(d)
+
+def BuildCommand(programPath):
+    # Input
+    command = r'"{}" .\forward\main.cpp /MD '.format(CompilerPath)
+    # Definitions
+    for d in Definitions:
+        command += AddDefinition(d)
+    command += AddDefinition(r'PROGRAM_PATH=LR\"({})\"'.format(programPath))
+    # Include Paths
+    for p in IncludesPaths:
+        command += AddIncludePath(p)
+    # Output
+    command += r'/Fe: {} '.format(os.path.basename(programPath))
+    # Lib Paths
+    command += r'/link /OPT:REF /OPT:ICF '
+    for p in LibPaths:
+        command += AddLibPath(p)
+
+    return command
 
 
 if __name__=='__main__':
@@ -10,13 +48,11 @@ if __name__=='__main__':
     programPath = sys.argv[1]
     if not os.path.exists(sys.argv[1]):
         raise RuntimeError("Program {} do not exist.".format(programPath))
-    build_command = CompilerPath + " " + '.\\forward\\main.cpp' + " "
-    build_command += '/D "PROGRAM_PATH=LR\\\"({})\\\""'.format(programPath) + " "
-    build_command += '/D "UNICODE"' + " "
-    build_command += '/D "USE_NAMED_MUTEX"' + " "
-    build_command += '/Fe: ' + os.path.basename(programPath)
-    print(build_command)
-    os.system(build_command)
+
+    command = BuildCommand(programPath)
+
+    # print(command)
+    subprocess.run(command)
     if os.path.exists("main.obj"):
         os.remove("main.obj")
     

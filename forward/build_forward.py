bf54779027f27b0d93b5394d2e2f84c4480086c6
tiruns <tiruns@yeah.net>
2022-10-19 17:04:16 +0800

    update build_forward.py

--- forward/build_forward.py
+++ forward/build_forward.py
@@ -10,34 +10,20 @@ Definitions = [
     r'NDEBUG',
     r'USE_NAMED_MUTEX'
 ]
-IncludesPaths = []
-LibPaths = []
-
-def AddIncludePath(path):
-    return r'/I "{}" '.format(path)
-
-def AddLibPath(path):
-    return r'/LIBPATH:"{}" '.format(path)
-
 def AddDefinition(d):
     return r'/D "{}" '.format(d)
 
 def BuildCommand(programPath):
     # Input
-    command = r'"{}" .\forward\main.cpp /MD '.format(CompilerPath)
+    command = r'"{}" .\main.cpp /MD '.format(CompilerPath)
     # Definitions
     for d in Definitions:
         command += AddDefinition(d)
     command += AddDefinition(r'PROGRAM_PATH=LR\"({})\"'.format(programPath))
-    # Include Paths
-    for p in IncludesPaths:
-        command += AddIncludePath(p)
     # Output
     command += r'/Fe: {} '.format(os.path.basename(programPath))
     # Lib Paths
     command += r'/link /OPT:REF /OPT:ICF '
-    for p in LibPaths:
-        command += AddLibPath(p)
 
     return command
 
